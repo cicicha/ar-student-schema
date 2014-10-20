9674b2ac211e1e740e93dd3cc25c6f7f1bffe04b
estelle <estelleccl@gmail.com>
2014-10-20 14:58:33 +0000

    completed AR Student Schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,20 @@
 require_relative '../../db/config'
 
+
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_uniqueness_of :email
+  validates_inclusion_of :age, in: 5..99
+  validates_length_of :phone, in: 10..32, numericality: { only_integer: true }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Time.now.year - birthday.year
+  end
+
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,19 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :gender
+      t.date  :birthday
+      t.datetime :created_at
+      t.datetime :updated_at
+      
+      t.timestamps
+    end
+      
   end
 end
+
